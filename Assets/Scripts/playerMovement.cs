48a04bdfd6d2c60bd2d9e7210c16261ed1f2e6d1
akashram98 <adiakash98@gmail.com>
2023-01-27 20:59:12 -0800

    playerMovement

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody pbody;
+    public float movementSpeed = 15f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 mag = new Vector3(horizontal, 0f, vertical).normalized;
+
+        Vector3 velocity = mag * movementSpeed * Time.deltaTime;
+
+        pbody.MovePosition(transform.position + velocity);
+        
+    }
+}
