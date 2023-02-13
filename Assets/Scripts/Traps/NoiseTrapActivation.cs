e2d50354bb5852b0666d9a865b47dab479fd0725
Aaron <agormley3424@gmail.com>
2023-02-12 17:18:45 -0800

    Fixed noise trap collision

--- Assets/Scripts/Traps/NoiseTrapActivation.cs
+++ Assets/Scripts/Traps/NoiseTrapActivation.cs
@@ -19,7 +19,7 @@ public class NoiseTrapActivation : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.gameObject.layer);
+        Debug.Log(other.gameObject.name);
         if (other.gameObject.layer == LayerMask.NameToLayer("Enemies"))
         {
             Debug.Log("Enemy Inside sphere");
