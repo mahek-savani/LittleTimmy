9664f98c1f8b14ed1dc6e3735ba094a25bbabd4a
Aaron <agormley3424@gmail.com>
2023-02-19 15:02:43 -0800

    Replaced old version of pit trap

--- Assets/Scripts/Traps/PitTrapButton.cs
+++ Assets/Scripts/Traps/PitTrapButton.cs
@@ -1,19 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.AI.Navigation;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class PitTrapButton : MonoBehaviour
 {
     public PitTrap pitTrap;
     public GameObject buttonParent;
     public MeshCollider door;
-    public GameObject trapDoor;
-    public GameObject fallTrigger;
-    //public NavMeshSurface navMesh;
-    //public NavMeshData currentNavMesh;
-
     public void OnTriggerEnter(Collider c)
     {
         if (c.gameObject.layer == 3)
@@ -25,24 +18,8 @@ public class PitTrapButton : MonoBehaviour
                 pitTrap.trapActive = false;
                 door.enabled = false;
                 data.trapActiveOrder.Add("pitTrap");
-                fallTrigger.SetActive(true);
-
-                trapDoor.layer = LayerMask.NameToLayer("Ignore Raycast");
-
-                //navMesh.RemoveData();
-                //navMesh.BuildNavMesh();
-
-                //navMesh.UpdateNavMesh(currentNavMesh);
-
                 //Destroy(gameObject);
             }
         }
     }
-
-    //IEnumerator rebuildNavMesh()
-    //{
-    //    yield return new WaitUntil(collisionChecked);
-    //    navMesh.RemoveData();
-    //    navMesh.BuildNavMesh();
-    //}
 }
