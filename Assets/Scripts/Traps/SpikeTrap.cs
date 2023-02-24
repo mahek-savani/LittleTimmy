7bb4a6a10e3ef95471a6f4d92b42aee81304e490
simransarin <sarinsimran3@gmail.com>
2023-02-24 01:47:06 -0800

    fixing traps

--- Assets/Scripts/Traps/SpikeTrap.cs
+++ Assets/Scripts/Traps/SpikeTrap.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class SpikeTrap : MonoBehaviour
 {
     public bool trapActive = true;
+    public GameObject trapParent;
     //forward direction
     float animDirection = 1.0f; 
     // Start is called before the first frame update
     public void playAnimation()
     {
         //changing spike trap's animation direction to forward
+        trapParent.GetComponent<Animation>().Play("SpikeTrapRot");
         gameObject.GetComponent<Animation>()["Spike Tutorial Hallway Anim"].speed = animDirection;
         gameObject.GetComponent<Animation>().Play("Spike Tutorial Hallway Anim");
     }
