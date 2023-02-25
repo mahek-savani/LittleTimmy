6a8e50865aabfa9e8650bbca71e894d91de2ba23
mahek-savani <maheksavani2000@gmail.com>
2023-02-24 16:30:10 -0800

    Clean Up

--- Assets/Scripts/Traps/BaseTrapClass.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BaseTrapClass : MonoBehaviour
-{
-    public string trapName;             // Used in UI to denote trap type
-    public bool isTriggered = false;    // Used for all pick-ups to decide if they're triggered
-}
