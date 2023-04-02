9b6fff4d50c325a43d4572965d44b590f365db12
simransarin <sarinsimran3@gmail.com>
2023-04-02 16:37:01 -0700

    level 1 changes tutorials

--- Assets/Scripts/Menu/PopUpSystem.cs
+++ Assets/Scripts/Menu/PopUpSystem.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PopUpSystem : MonoBehaviour
 {
     public GameObject popUpBox;
     public Animator animator;
     public TMP_Text popUpText;
+    public Image popUpImage;
 
     public void PopUp(string text)
     {
         popUpBox.SetActive(true);
         popUpText.text = text;
+        animator.SetTrigger("pop");
+        Time.timeScale = 0;
+    }
+
+    public void PopUpImage(Sprite image)
+    {
+        popUpBox.SetActive(true);
+        popUpImage.sprite = image;
+        popUpText.text = "";
+        // popUpBox.GetComponentInChildren<Image>().sprite = ;
         animator.SetTrigger("pop");
         Time.timeScale = 0;
     }
