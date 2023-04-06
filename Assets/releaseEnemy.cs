982d2be0a88cb2276e016cfcb4575fa07ebc34bd
akashram98 <adiakash98@gmail.com>
2023-04-05 18:37:48 -0700

    level_2_update

--- Assets/releaseEnemy.cs
+++ Assets/releaseEnemy.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class releaseEnemy : MonoBehaviour
 {
     public GameObject gate;
-    public GameObject npcFake;
-    public GameObject npcReal;
+    //public GameObject npcFake;
+    //public GameObject npcReal;
     private void OnTriggerEnter(Collider other)
     {
 
         if (other.gameObject.layer == 3)
         {
             gate.SetActive(false);
-            npcFake.SetActive(false);
-            npcReal.SetActive(true);
+            //npcFake.SetActive(false);
+            //npcReal.SetActive(true);
         }
     }
 }
